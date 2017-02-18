b1165b2d2fccc03ddf1a56df8a41f0f699e50fef
wancong <wancongzhang@gmail.com>
2017-02-18 15:53:31 -0500

    delete window.store

--- frontend/chatroom.jsx
+++ frontend/chatroom.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
 import Root from './components/root';
 
-import { signup, login, logout } from './actions/session_actions';
-import { fetchLastMessage, fetchMessages, fetchUnseenMessages, createMessage } from './actions/message_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
     let store;
 
@@ -15,13 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       store = configureStore();
     }
-
-    window.store = store;
-    window.fetchLastMessage = fetchLastMessage;
-    window.fetchMessages = fetchMessages;
-    window.createMessage = createMessage;
-    window.fetchUnseenMessages = fetchUnseenMessages;
-    window.login = login;
 
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
